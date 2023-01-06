b031e3973ba347909c9676f17d2120375e2f0c53
boyan hb <bobibonev1@abv.bg>
2023-01-07 01:23:52 +0200

    Changed innerBoxes to Box*, improved print and fixed removeUselessBoxes

--- box.cpp
+++ box.cpp
@@ -1,31 +1,69 @@
 #include "box.h"
+#include <iostream>
+
+void Box::print (std::ostream& os) const {
+    if (!name.empty()) {
+        os << name << " -> ";
+        if (!souvenirs.empty()) {
+            std::vector<std::string> copy = souvenirs;
+            while (!copy.empty()) {
+                os << copy.back() << ' ';
+                copy.pop_back();
+            }
+            
+        }
+        if (!innerBoxes.empty()) {
+            std::vector<Box*> boxesCopy = innerBoxes;
+            while (!boxesCopy.empty()) {
+                os << "( ";
+                boxesCopy.back()->print(os);
+                os << ") ";
+                boxesCopy.pop_back();
+            }
+        }
+    }
+}
 
 void Box::removeUselessBoxes () {
     if (innerBoxes.empty()) {
         return;
     }
-    std::vector<Box>::iterator it = innerBoxes.begin();
-    /*
-    continues the cycle after adding boxes when there is only a box
-    */
+    std::vector<Box*>::iterator it = innerBoxes.begin();
+    // continues the cycle after adding boxes when there is only a box
     while (it != innerBoxes.end()) {
-        if ((*it).empty())
-            // deletes current and moves to the next
-            it =innerBoxes.erase(it);
-        else if ((*it).hasOnlyABox()) {
+    // the iterators go to random memory whenever they use erase() or push_back() 
+    // so inconveniently it has to go to the beginning of the boxes everytime
+    // it either removes an element 
+    // or inserts a new one
+        if ((*it)->empty()) {
+            innerBoxes.erase(it);
+            it = innerBoxes.begin();
+            /*
+            betterCase:
+            innerBoxes.erase(it++);
+            */
+        }
+        else if ((*it)->hasOnlyABox()) {
             // push back the only box the current innerBox has
-            innerBoxes.push_back ( (*it).innerBoxes.front() );
-            // deletes current and moves to the next
-            it = innerBoxes.erase(it);
+            Box* toInsert = (*it)->innerBoxes.front();
+            (*it)->innerBoxes.clear();
+            innerBoxes.push_back ( toInsert );
+            it = innerBoxes.begin();
+            /*
+            betterCase:
+            innerBoxes.push_back((*it)->innerBoxes.front());
+            innerBoxes.erase(it++);
+            */
+
         }
         else {
             // goes deeper into the box because it has more than one box and isn't empty
-            (*it).removeUselessBoxes();
+            (*it)->removeUselessBoxes();
             // if after clearing the useless boxes, the box is still useful
-            //                                has a souvenir or more than 2 useful boxes
+            //                                (has a souvenir or more than 2 useful boxes)
             // then move to the next box
             // else check again if its empty or has a single box
-            if ((*it).usefulBox())
+            if ((*it)->usefulBox())
                 it++;
         }
     }
--- box.h
+++ box.h
@@ -7,12 +7,16 @@
 class Box  {
     std::string name;
     std::vector<std::string> souvenirs;
-    std::vector<Box> innerBoxes;
+    std::vector<Box*> innerBoxes;
 public:
     Box(std::string const& _name) : name(_name) {}
     std::string getName() const {return name;}
+    void print (std::ostream& os = std::cout) const;
     void addSouvenir(std::string const& souvenirName) { souvenirs.push_back(souvenirName); }
-    void insertBox (Box const& boxName) { innerBoxes.push_back(boxName); }
+    void insertBox (Box& boxName) { 
+        Box* toInsert = &boxName;
+        innerBoxes.push_back(toInsert);
+    }
     bool empty() const { return souvenirs.empty() && innerBoxes.empty(); }
     bool hasOnlyABox () const { return souvenirs.empty() && innerBoxes.size() == 1; }
     bool usefulBox () const { return !souvenirs.empty() || innerBoxes.size() > 1; }
